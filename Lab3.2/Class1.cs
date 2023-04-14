53cac553d00e148235afd896eb1230c281a01f59
Hlebsmaslo <94301600+Hlebsmaslo@users.noreply.github.com>
2023-04-14 22:41:31 +0300

    Add files via upload

---
+++ Lab3.2/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3._2
+{
+    class Class1
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ Lab3.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System;
+using System.Text.Json;
+using Lab3._2;
+using System.Linq;
+
+namespace Lav3._2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string pd = @"path";
+            Func<Class1, bool> fil = p => p.Price > 50;
+            for (int i = 1; i <= 10; i++)
+            {
+                string fp = Path.Combine(pd, $"{i}.json");
+
+                var prod = JsonConvert.DeserializeObject<List<Class1>>(File.ReadAllText(fp));
+                List<Class1> filtered = prod.Where(fil).ToList();
+                filtered.ForEach(p => Console.WriteLine(p.Name));
+            }
+        }
+    }
+}
